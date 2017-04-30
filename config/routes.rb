63ade5cc8e9dd5981589b3d1f12cb4a8bbf718fa
Phillip Simmonds <phillip.j.simmonds@gmail.com>
2017-04-30 22:58:31 +1000

    Refactored routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'teachers', to: 'teachers#index'
-  get 'teachers/:id', to: 'teachers#show', as: 'teacher'
-
-  get 'students', to: 'students#index'
-  get 'students/:id', to: 'students#show', as: 'student'
-  get 'students/:id/edit', to: 'students#edit', as: 'edit_student'
-  put 'students/:id', to: 'students#update'
-  patch 'students/:id', to: 'students#update'
+  resources :students, except: %i[new create destroy]
+  resources :teachers, only: %i[index show]
 
   root 'public#index'
 end
